07c4868622c3b4902d0aef2e8841c3a3a4fa8bfa
Lucas Lois <lucas.lois@privy.io>
2025-08-26 08:24:58 -0300

    feat: ethereum signMessage (#6)
    DRI reviewer: @jagtejsodhi @ankushswar1 
    
    ## Summary
    
    Implements signing messages with ETH wallets via `personal_sign`
    
    ### Testing
    
    Integration tests added.

--- src/public-api/services/wallets.ts
+++ src/public-api/services/wallets.ts
@@ -1,3 +1,24 @@
-import { Wallets } from '../../resources';
+import { PrivyAPI } from '../../client';
+import { WalletRpcParams, WalletRpcResponse, Wallets } from '../../resources';
+import { PrivyEthereumService } from './ethereum';
 
-export class PrivyWalletsService extends Wallets {}
+export class PrivyWalletsService extends Wallets {
+  private ethereumService: PrivyEthereumService;
+
+  constructor(privyApiClient: PrivyAPI) {
+    super(privyApiClient);
+    this.ethereumService = new PrivyEthereumService(this);
+  }
+
+  public ethereum(): PrivyEthereumService {
+    return this.ethereumService;
+  }
+
+  public async rpc<Params extends WalletRpcParams>(
+    walletId: string,
+    params: Params,
+  ): Promise<Extract<WalletRpcResponse, { method: Params['method'] }>>;
+  public async rpc(walletId: string, params: WalletRpcParams): Promise<WalletRpcResponse> {
+    return await this._rpc(walletId, params);
+  }
+}
